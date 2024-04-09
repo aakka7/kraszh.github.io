1c288cbf322e09492c26e89f705e6a8b692dbf86
Amir Mamadaliev <157823299+kraszh@users.noreply.github.com>
2024-04-08 23:21:11 -0400

    rm preventDef

--- dform.js
+++ dform.js
@@ -1,7 +1,6 @@
 const form = document.querySelector('form');
 form.addEventListener('continue', (e) => {
-    e.preventDefault();
-        
+
     let diets = [];
     document.querySelectorAll('[type = "checkbox"]').forEach(item => {
         if(item.checked === true){
