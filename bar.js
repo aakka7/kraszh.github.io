0aa37d39c6b9625b5ca128598ab3c4caef80a078
Amir Mamadaliev <157823299+kraszh@users.noreply.github.com>
2024-04-10 19:19:02 -0400

    add task

--- bar.js
+++ bar.js
@@ -2,6 +2,7 @@ let input  = document.querySelector('.IngIn');
 let addBtn  = document.querySelector('.btn');
 let ing  = document.querySelector('.ing');
 
+//Add button disabled
 input.addEventListener('keyup', () => {
     if(input.ariaValueMax.trim() !== 0){
         addBtn.classList.add('active');
@@ -10,4 +11,22 @@ input.addEventListener('keyup', () => {
         addBtn.classList.remove('active');
     }
 
-})
+});
+
+//Add ing
+
+addBtn.addEventListener('click', () => { 
+    if(input.ariaValueMax.trim() !== 0){
+        let newIng = document.createElement('div');
+        newIng.classList.add('Ing');
+        newIng.innerHTML = '
+        <p> ${input.value}</p>
+        <div class = "item-btn">
+            <i class = "remove"></i>
+        </div>
+        ';
+        ing.appendChild(newIng);
+        input.value = ' ';
+    }
+
+});
