2f3253152531b1fd9eb5458d995bcf5b54cae20e
Amir Mamadaliev <157823299+kraszh@users.noreply.github.com>
2024-04-12 21:31:09 -0400

    load ings

--- bar.js
+++ bar.js
@@ -13,6 +13,27 @@ input.addEventListener('keyup', () => {
 })
 
 //Add ing
+function saveI() {
+    var ingsList = [];
+    if(input.value.trim() != 0){
+        let newIng = document.createElement('div');
+        newIng.classList.add('ing');
+        newIng.innerHTML = `
+        <p>${input.value}</p> 
+        <div class = "item-btn"> 
+            <i class="fa-solid fa-circle-xmark"></i> 
+        </div>
+        <br>`;
+        ings.appendChild(newIng);
+        ingsList.push(item.value);
+        var ings_string = JSON.stringify(ingsList);
+        localStorage.setItem("ings_string", ings_string);
+        input.value = '';
+    }
+    else {
+        alert("empty");
+    }
+}
 addBtn.addEventListener('click', () => { 
     if(input.value.trim() != 0){
         let newIng = document.createElement('div');
