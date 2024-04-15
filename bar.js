221a585d9d18d07280cd09e4342ceecadad4837a
Amir Mamadaliev <157823299+kraszh@users.noreply.github.com>
2024-04-14 23:27:25 -0400

    button looks

--- bar.js
+++ bar.js
@@ -47,7 +47,7 @@ function load() {
             newIng.innerHTML = `
             <p>${retList[item]}</p> 
             <div class = "item-btn"> 
-                <input type = "button" class="in" value = "${retList[item]}"><i class = "fa-solid fa-circle-xmark"></i> </input> 
+                <input type = "button" class="in" value = "${retList[item]}"><img src = "fa-solid fa-circle-xmark"></i> </input> 
             </div>`;
             ings.appendChild(newIng);
         }
