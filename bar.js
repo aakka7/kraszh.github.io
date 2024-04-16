ca882f4da06e0e906045da38c560ee475bbe8fe2
Amir Mamadaliev <157823299+kraszh@users.noreply.github.com>
2024-04-16 15:08:19 -0400

    separated by commas function

--- bar.js
+++ bar.js
@@ -12,9 +12,40 @@ input.addEventListener('keyup', () => {
     }
 })
 
+function addList(str){
+    var arr = str.split(', ');
+    console.log(arr);
+    for(element in arr){
+        arr[element] = arr[element].trim().toUpperCase();
+        var checkList = JSON.parse(localStorage.getItem('ings_string'));
+        if(!checkList.includes(input.value)){   
+            let newIng = document.createElement('div');
+            newIng.classList.add('ing');
+            newIng.innerHTML = `
+            <p>${arr[element]}</p> 
+            <div class = "item-btn"> 
+                <input type = "button" class="in" value = "${arr[element]}"><i class = "fa-solid fa-circle-xmark"></i></input>
+            </div>`;
+            ings.appendChild(newIng);
+            /** Rebuilds the list in case something was removed **/
+            var tempList = [];
+            document.querySelectorAll('[class = "in"]').forEach(item => {
+                tempList.push(item.value);
+            });
+            var tempString = JSON.stringify(tempList);
+            console.log(tempString);
+            localStorage.setItem('ings_string', JSON.stringify(tempList));
+    }
+    input.placeholder = '    Add Ingredient';
+    input.value = '';
+}
 //Add ing
 function saveI() {
-    if(input.value.trim() != 0){
+    if(input.value.includes(',')){
+        addList(input.value);
+    }
+    else if(input.value.trim() != 0){
+        input.value = input.value.trim();
         input.value = input.value.toUpperCase();
         var checkList = JSON.parse(localStorage.getItem('ings_string'));
         if(!checkList.includes(input.value)){    
@@ -26,13 +57,12 @@ function saveI() {
                 <input type = "button" class="in" value = "${input.value}"><i class = "fa-solid fa-circle-xmark"></i></input>
             </div>`;
             ings.appendChild(newIng);
-            
+            /** Rebuilds the list in case something was removed **/
             var tempList = [];
             document.querySelectorAll('[class = "in"]').forEach(item => {
                 tempList.push(item.value);
             });
             var tempString = JSON.stringify(tempList);
-            console.log(tempString);
             localStorage.setItem('ings_string', JSON.stringify(tempList));
             input.placeholder = '    Add Ingredient';
             input.value = '';
@@ -72,7 +102,6 @@ ings.addEventListener('click', (e) => {
             tempList.push(item.value);
         });
         var tempString = JSON.stringify(tempList);
-        console.log(tempString);
         localStorage.setItem('ings_string', JSON.stringify(tempList));
     }
 
